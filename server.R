707d71585cdf1b7a91196611ef04b3c12c0497cb
Juan Quer Martinez <juan@MacBook-Pro-de-Juan.local>
2021-03-06 12:33:42 +0100

    first commit

---
+++ server.R
@@ -0,0 +1,172 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(pdfetch)
+library(ggplot2)
+library(tidyverse)
+library(xts)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    ## VALORES DINAMIOCS ##
+    # Para el buen funcionamiento de la APP, hay que usar valores dinámicos.
+    
+    
+    # PINICIALIZACION DEL CURSOR
+    values<- reactiveValues(
+        x_value=as.Date("2017-09-06"),
+        y_value=80
+    )
+    
+    ranges <- reactiveValues(x = c(as.Date("2015-09-09"),as.Date("2020-09-09")), y = NULL )
+    ranges_or <- reactiveValues(x = c(as.Date("2015-09-09"),as.Date("2020-09-09")), y = NULL )
+    
+    observeEvent(input$dateRange1, {
+        ranges_or$x <-  c(as.Date(input$dateRange1[1]),as.Date(input$dateRange1[2]))
+        ranges_or$y <- NULL
+        ranges$x <-  c(as.Date(input$dateRange1[1]),as.Date(input$dateRange1[2]))
+        ranges$y <- NULL
+        }
+        )
+    
+    # INICIALIZACION RANGO FECHAS MAPA
+    
+   
+    # CUROSRES EN FUCNION DEL DBL CLICK
+    observeEvent(
+        input$plot_dblclick, {
+        values$x_value <- input$plot_dblclick$x
+        values$y_value <- input$plot_dblclick$y
+        }
+        
+    )
+    
+    ## ZOOM
+    observeEvent(input$plot_click, {
+        brush <- input$plot1_brush
+        if (!is.null(brush)) {
+            ranges$x <- c(as.Date(brush$xmin), as.Date(brush$xmax))
+            ranges$y <- c(brush$ymin, brush$ymax)
+            
+            
+        } else {
+            ranges$x <- ranges_or$x
+            ranges$y <- NULL
+        }
+    })
+
+    
+    
+    
+    # SHOW del grafico
+    output$distPlot <- renderPlot({
+        
+        # Nombre de la empresa
+        empresa = input$empresa1
+        # Hacemos una peticion a los datos
+        datos <- pdfetch_YAHOO(empresa,
+                               fields = c("open", "high", "low", "close", "adjclose", "volume"), from = as.Date(input$dateRange1[1]),
+                               to = as.Date(input$dateRange1[2]), interval = "1d")
+        
+        #ranges$x <- c(as.Date(input$dateRange1[1]),as.Date(input$dateRange1[2]))
+        # Lo transformamos a un DF para poder trabajar bien
+        df = data.frame(date=as.Date(index(datos)), datos)
+        # Renombramos las columnas dinamicamente para que se escalable
+        df=df %>% 
+            rename(
+                high = paste(empresa,".high",sep=""),
+                low = paste(empresa,".low",sep=""),
+                close = paste(empresa,".close",sep=""),
+                open = paste(empresa,".open",sep=""),
+                adjClose = paste(empresa,".adjclose",sep=""),
+                volume = paste(empresa,".volume",sep="")
+            )
+        assign("df_total", df, envir = .GlobalEnv)
+        # Creamos los datos para las barras rojas y verdes. Este primer paso solo es el valor
+        df$barras = df$high - df$low
+        # Ahora si uq ele metemos la variable del color, que depende de si ha cerrado más arriba de lo que ha abierto
+        df$color_barra = ifelse(sign(df$close - df$open)>0,"#10a326","#a31710")
+        
+        if(input$displayLines1)
+        {
+            HICP <- ggplot() +
+                # Pintamos la linea de cierre
+                
+                geom_segment(data = df,aes(x=date, xend=date, y=low, yend=high), size=0.4, color="black") +
+                geom_segment(data = df,aes(x=date, xend=date, y=close, yend=open), size=2, color=df$color_barra) +
+                geom_line(data = df, aes(x=date, y = close), size = 0.5, alpha=0.7, color="#0af2ee") +
+                geom_vline(xintercept = as.Date(values$x_value), linetype="longdash", color = "#d8b2ed", size=1) + 
+                coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE) +
+                theme_light()
+            HICP   
+        }else{
+            HICP <- ggplot() +
+                # Pintamos la linea de cierre
+                
+                geom_segment(data = df,aes(x=date, xend=date, y=low, yend=high), size=0.4, color="black") +
+                geom_segment(data = df,aes(x=date, xend=date, y=close, yend=open), size=2, color=df$color_barra) +
+                geom_vline(xintercept = as.Date(values$x_value), linetype="longdash", color = "#d8b2ed", size=1) +
+                coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = FALSE) +
+                theme_light()
+            HICP
+        }
+        
+
+
+        })
+    
+    
+    output$textInfo1 <- renderText({
+        
+        seleccionado = df_total %>% filter(date==toString(as.Date(values$x_value)))
+        
+        paste0("<h4>Detalle: </h4>",
+               "<p> <b> Fecha: </b>", seleccionado$date, "</p>",
+               "<p> <b>Cierre: </b>",round(seleccionado$close,2), "</p>",
+               "<p> <b>Apertura: </b>",round(seleccionado$open,2), "</p>",
+               "<p> <b>Maximo: </b>",round(seleccionado$high,2),"</p>",
+               "<p> <b>Minimo: </b>",round(seleccionado$low,2),"</p>",
+               "<p> <b>Adj: </b>",round(seleccionado$adjClose,2),"</p>")
+
+            
+        
+    })
+    
+    output$table1 <- renderTable({
+        df_total
+    })
+    
+    output$imagen <- renderImage({
+        
+            return(list(
+                src = "velas.png",
+                contentType = "image/png",
+                alt = "Velas"
+            ))
+        
+        
+    }, deleteFile = FALSE)
+    
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste(input$empresa1,"-", Sys.Date(), ".csv", sep="")
+        },
+        content = function(file) {
+            write.csv(df_total, file)
+        }
+    )
+
+    
+    
+     
+    
+
+})
---
+++ ui.R
@@ -0,0 +1,107 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyWidgets)
+
+color1="#9bcfe8"
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("IBEX 35 Comparative"),
+    
+    ## PRIMER GRAFICO DE ARRIBA
+    fluidRow(
+        # Parametros el grafico
+        column(3,
+               wellPanel(
+                   style = paste("background:",color1),
+                   # Empresa del IBEX
+                   selectInput(inputId ="empresa1",
+                                   label="Selecciona una de las empresas del IBEX35",
+                                   choices=c("ANA.MC","IBE.MC","ACX.MC","ACS.MC","AENA.MC","ALM.MC","ITX.MC","SAN.MC","BBVA.MC","AMS.MC","CLNX.MC","TEF.MC","FER.MC","CABK.MC","REP.MC","GRF.MC","ELE.MC","RED.MC","IAG.MC","SGRE.MC","NTGY.MC","ENG.MC","MTS.MC","BKT.MC","MRL.MC","MAP.MC"),
+                                   selected="ANA.MC"
+                   ),
+                   # Rango de fechas
+                   dateRangeInput("dateRange1","Enter Start and End Dates for the Display",
+                                  start="2015-01-01",end=Sys.Date(), min="2015-01-01", max=Sys.Date(),
+                                  format="yyyy-mm-dd",),
+                   # Descargar datos
+                   downloadButton("downloadData","Descargar datos"),
+                   # Rerpesentar grafico de lineeas
+                   checkboxInput("displayLines1",label = "Representar lineas"),
+               )       
+        ),
+        column(6,
+               
+               withTags({
+                   div(class="header", checked=NA,
+                       p("Esta práctica tiene el objetivo de representar multiples gráficos y tablas que puedan ser útiles a la hora de decidir si invertir en 
+               una determinada empresa del IBEX35. Desde el panel de la izquierda, se puede seleccionar cualquier empresa, y el rango de fechas que se
+               pretende analizar.)"),
+               p("La etiqueta Gráfico, representa uno de los gráficos más importantes que se emplean en este nicho. Es bastante
+                parecido a un Boxplot, aquí en lineas negras se representa la variación entre el máximo y mínimo, y con líneas rojas o verdes la diferencia 
+               entre la apertura y el cierre. (Si el cierre es más elevado que la apertura, será verde, y rojo en el caso contrario). Resumen gráfico en la siguiente imagen"),
+               
+               p("Haciendo doble click sobre un gráfico, pordemos obtener más información para el día seleccionado, apareciendo la información en más detalle en
+               el margen derecho del gráfico. Para hacer Zoom, basta con seleccionar el área sobre el que se pretende ampliar y hacer click sobre ella. Para hacer
+               Zoom Out, simplemente hay que hacer un click sobre cualquier punto del gráfico."),
+               
+               
+                       a(href="https://github.com/juanquer/shiny_stock.git", "GitHub!")
+                   )
+               })
+               
+                   
+        ),
+        
+        column(3,
+                imageOutput("imagen"),
+               
+        ),
+    ),
+    
+    fluidRow(
+    column(10,
+           
+           tabsetPanel(
+               tabPanel("Grafico", 
+                        div(style='height:500px; overflow: scroll',plotOutput("distPlot",
+                                                                              click = "plot_click",
+                                                                              dblclick = "plot_dblclick",
+                                                                              brush = brushOpts(
+                                                                                  id = "plot1_brush",
+                                                                                  resetOnNew = TRUE
+                                                                              )))
+               ),
+               tabPanel("Tabla",div(style='height:300px; overflow: scroll',
+                                    tableOutput("table1")
+               ))
+           ),
+           
+           
+           
+    ),
+    column(2,
+           div(style="height:50px;"),
+           wellPanel(style = paste("background:",color1),
+                     htmlOutput("textInfo1")
+           )  
+    ),
+    )
+    
+    
+
+ 
+
+   
+    
+))
